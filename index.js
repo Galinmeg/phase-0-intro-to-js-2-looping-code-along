936f6d42e7e1b7b238a24e46c698f396307063f9
Megan Galinis <megan.galinis@gmail.com>
2023-03-09 08:33:54 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names , string) {
+    const messageNames = []
+    for(let i = 0 ; i < names.length; i++)
+    {
+    messageNames.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+    return messageNames;
+}
+
+function countDown() {
+    let countDown = 0;
+    while (countDown < 11) {
+        console.log(countDown);
+        countDown ++;
+    }
+}
+
+countDown(4)
+
+
+
+
+
+
+
